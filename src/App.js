d22c2b6eaf8553547c80904817d5acda62eb6798
draden <hanshengliang@outlook.com>
2023-03-20 17:53:09 +0800

    :zap: Add iframe code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+    const [url, setUrl] = useState(null);
+    const [showIFrame, setShowIFrame] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        {
+            !showIFrame ? (
+                <>
+                    <input onChange={(e) => setUrl(e.target.value)} />
+                    <button onClick={() => setShowIFrame(true)}>Submit</button>
+                </>
+            ) : (
+                <iframe src={url} height={"500vh"} width={"100%"} allow="camera;fullscreen;accelerometer;gyroscope;magnetometer" allowFullScreen />
+            )
+        }
+    </>
   );
 }
 
